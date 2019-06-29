a7607ab12d214be9c6acf20b2d0f45f38623d476
Rik Duijm <rikduijm@gmail.com>
2019-06-29 15:54:24 +0000

    first commitment

---
+++ js/my_script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("th").click(function() {
+        $(this).addClass("selection");
+    });
+
+
+});
+
+
+
+/*    $("tr:odd").addClass("odd"); 
+    $("tr:even").addClass("even"); 
+});
+*/
